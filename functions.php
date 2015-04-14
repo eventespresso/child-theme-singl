09c577971ed25c942a89a02926b1d50ac6a7add8
Josh Feck <josh@nxnweb.com>
2015-04-14 17:03:11 -0400

    Remove background image and change default background color.

---
+++ functions.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Singl functions and definitions
+ *
+ * @package Singl
+ */
+
+/**
+ * Set up a custom default background.
+ */
+add_action ( 'after_setup_theme', 'ee_check_blog_registered_on_after_setup_theme' );
+function ee_check_blog_registered_on_after_setup_theme() {
+	if ( function_exists( 'get_blog_details' )) {
+		$details = get_blog_details( get_current_blog_id() );
+		if ( $details->registered > '2015-04-16 00:00:00' ) { // only change the default if this is a new site
+			add_filter( 'singl_custom_background_args', 'ee_single_custom_background_args' );
+		}
+	}
+}
+
+function ee_single_custom_background_args() {
+	$defaults = array(
+		'default-position'   => 'center',
+		'default-repeat'     => 'no-repeat',
+		'default-attachment' => 'fixed',
+		'default-image'      => '',
+		'default-color'      => '61b1d8',
+	);
+	return $defaults;
+}
